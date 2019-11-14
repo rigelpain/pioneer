2d6a4d0d60f0246ec7843847b33977f0c1cedc00
Zundagit <zundamochi1715944@ezweb.ne.jp>
2019-11-14 09:39:29 +0900

    背景追加

--- pioneer/MyWorld.java
+++ pioneer/MyWorld.java
@@ -18,4 +18,11 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
     }
+    
+     public void act() 
+    {
+            GreenfootImage img1 = new GreenfootImage( "images/background1.png" );
+            img1.scale(600,435);
+            getBackground().drawImage( img1, 0, 0 );
+    }    
 }
