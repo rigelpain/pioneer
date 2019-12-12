ed347888fe41ad61ab01a539024d53507c00473b
yoripy <15024@hakodate.kosen-ac.jp>
2019-12-12 09:16:05 +0900

    playerに動きを追加

--- pioneer/PLAYER.java
+++ pioneer/PLAYER.java
@@ -8,34 +8,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class PLAYER extends Actor
 {
-    int count=0;
     private  GreenfootImage img_right1 = null;
-    private  GreenfootImage img_right2 = null;
-    private  GreenfootImage img_right3 = null;
     private  GreenfootImage img_hidari1 = null;
-    private  GreenfootImage img_hidari2 = null;
-    private  GreenfootImage img_hidari3 = null;
     private  GreenfootImage img_back1 = null;
-    private  GreenfootImage img_back2 = null;
-    private  GreenfootImage img_back3 = null;
     private  GreenfootImage img_kousin1 = null;
-    private  GreenfootImage img_kousin2 = null;
-    private  GreenfootImage img_kousin3 = null;
     
     public PLAYER()
     {
          img_right1 =  new GreenfootImage( "images/right1.png" );
-         img_right2 =  new GreenfootImage( "images/right2.png" );
-         img_right3 =  new GreenfootImage( "images/right3.png" );
          img_hidari1 =  new GreenfootImage( "images/hidari1.png" );
-         img_hidari2 =  new GreenfootImage( "images/hidari2.png" );
-         img_hidari3 =  new GreenfootImage( "images/hidari3.png" );
          img_back1 =  new GreenfootImage( "images/back1.png" );
-         img_back2 =  new GreenfootImage( "images/back2.png" );
-         img_back3 =  new GreenfootImage( "images/back3.png" );
          img_kousin1 =  new GreenfootImage( "images/kousin1.png" );
-         img_kousin2 =  new GreenfootImage( "images/kousin2.png" );
-         img_kousin3 =  new GreenfootImage( "images/kousin3.png" );
     }
     
     /**
@@ -48,38 +31,31 @@ public class PLAYER extends Actor
         int y = getY();
         //getImage().scale( 100, 100 );
         if( Greenfoot.isKeyDown( "right" ) ){
-            setLocation( x+1,y );
-            if(count<12)setImage(img_right1);
-            
-            if(count<18)setImage(img_right2);
-            
-            if(count<24)setImage(img_right3);           
+            setLocation( x+2,y );
+            setImage(img_right1);
+                     
         }
         if( Greenfoot.isKeyDown( "left" ) ){
-            setLocation( x-1,y );
-            if(count<12)setImage(img_hidari1);
-            if(count<18)setImage(img_hidari2);
-            if(count<24)setImage(img_hidari3);
+            setLocation( x-2,y );
+            setImage(img_hidari1);
+           
         }
         if( Greenfoot.isKeyDown( "up" ) ){
-            setLocation( x,y-1 );
-            if(count<12)setImage(img_back1);
-            if(count<18)setImage(img_back2);
-            if(count<24)setImage(img_back3);
+            setLocation( x,y-2 );
+            setImage(img_back1);
+           
             
         }
         if( Greenfoot.isKeyDown( "down" ) ){
-        setLocation( x,y+1 );
-            if(count<12)setImage(img_kousin1);
-            if(count<18)setImage(img_kousin2);
-            if(count<24)setImage(img_kousin3);
+        setLocation( x,y+2 );
+           setImage(img_kousin1);
+          
         }
         Actor actor = getOneObjectAtOffset( 0, 0, enemy.class );
         if( actor != null ){
             getWorld().showText( "HOGE", 100, 50 );
         }  
-        count++;
-        if(count==24)count=1;
+       
     }    
         
        /* if(getX() == 0 || getX() == 400){
